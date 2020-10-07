300a7b9c7e4a4460b014a6a593eddfbe1736076e
Kyle Naumann <67936151+kyle-naumann@users.noreply.github.com>
2020-10-06 20:21:59 -0700

    MVP, and Styled for stretch. Added a hide show button for the password.

---
+++ user-onboarding/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Form from './Components/Form';
+import './App.css';
+
+function App() {
+  return (
+  <div className= "App">
+    <h1>Please Sign Up</h1>
+    <Form />
+  </div>
+  );
+}
+
+export default App;
